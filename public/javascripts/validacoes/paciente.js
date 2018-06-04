e225a9a432202c920132efa0c42892128adf4217
André Carlos <andre.carlos1993@hotmail>
2018-06-04 13:07:49 -0300

    Alterando db

--- public/javascripts/validacoes/paciente.js
+++ public/javascripts/validacoes/paciente.js
@@ -5,76 +5,112 @@ $("#telefone").mask("(99)999999999");
 function validarCampos() {
     // Validando nome do paciente
     if ($("#nome").val() == "") {
-        bootbox.alert({message: "Por favor, informe o seu nome.", size: 'small'});
+        bootbox.alert({
+            message: "Por favor, informe o seu nome.",
+            size: 'small',
+            callback: function () {
+                $(function() { $("#nome").focus(); });
+            }
+        });
         return false;
     }
 
     // Validando CPF
     if ($("#cpf").val() == "") {
-        bootbox.alert({message: "Por favor, informe o seu CPF.", size: 'small'});
+        bootbox.alert({
+            message: "Por favor, informe o seu CPF.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#cpf").focus(); });
+            }
+        });
         return false;
     } else if (!isValidCPF($("#cpf").val())) {
-        bootbox.alert({message: "CPF informado é inválido.", size: 'small'});
+        bootbox.alert({
+            message: "CPF informado é inválido.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#cpf").focus(); });
+            }
+        });
         return false;
     }
 
     // Validando data nascimento
     if ($("#dataNascimento").val() == "") {
-        bootbox.alert({message: "Por favor, informe sua data de nascimento.", size: 'small'});
+        bootbox.alert({
+            message: "Por favor, informe sua data de nascimento.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#dataNascimento").focus(); });
+            }
+        });
         return false;
     } else if (!isValidData($("#dataNascimento").val())) {
-        bootbox.alert({message: "Data de nascimento informada é inválida.", size: 'small'});
+        bootbox.alert({
+            message: "Data de nascimento informada é inválida.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#dataNascimento").focus(); });
+            }
+        });
         return false;
     }
 
     // Validando email
     if ($("#email").val() == "") {
-        bootbox.alert({message: "Por favor, informe seu E-mail.", size: 'small'});
+        bootbox.alert({
+            message: "Por favor, informe seu E-mail.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#email").focus(); });
+            }
+        });
         return false;
     } else if (!isValidEmail($("#email").val())) {
-        bootbox.alert({message: "E-mail informado é inválido.", size: 'small'});
+        bootbox.alert({
+            message: "E-mail informado é inválido.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#email").focus(); });
+            }
+        });
         return false;
     }
 
     // Validando telefone
     if ($("#telefone").val() == "") {
-        bootbox.alert({message: "Por favor, informe seu telefone.", size: 'small'});
+        bootbox.alert({
+            message: "Por favor, informe seu telefone.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#telefone").focus(); });
+            }
+        });
         return false;
     } else if (!isValidTelefone($("#telefone").val())) {
-        bootbox.alert({message: "Telefone informado é inválido.", size: 'small'});
+        bootbox.alert({
+            message: "Telefone informado é inválido.", 
+            size: 'small',
+            callback: function () {
+                $(function() { $("#telefone").focus(); });
+            }
+        });
         return false;
     }
     return true;
 }
 
-$( "#createPaciente" ).submit(function( event ) {
+$("#formPaciente").submit(function( event ) {
     if (validarCampos()) 
-        $( "#salvar" ).submit();
+        $("#salvar").submit();
     else 
         event.preventDefault();
 });
 
-$("#nome").keypress(function () {
-    $("#nome").removeClass("is-invalid");
-    $("#inNome").removeClass("block").addClass("none");
-});
-
-$("#cpf").keypress(function () {
-    $("#cpf").removeClass("is-invalid");
-    $("#inCpf").removeClass("block").addClass("none");
-});
-
-$("#dataNascimento").blur(function () {
-    $("#dataNascimento").removeClass("is-invalid");
-    $("#inDataNascimento").removeClass("block").addClass("none");
-});
-
-$("#email").blur(function () {
-    $("#email").removeClass("is-invalid");
-    $("#inEmail").removeClass("block").addClass("none");
-});
-
-$("#telefone").blur(function () {
-    $("#telefone").removeClass("is-invalid");
-    $("#inTelefone").removeClass("block").addClass("none");
-});
+// Para tratar a msg enviada do banco (layout - msg)
+if( $("#msg").val() != '' ) {
+    bootbox.alert($("#msg").val(), function(){
+        $(function() { $("#cpf").focus(); });
+    });
+}
